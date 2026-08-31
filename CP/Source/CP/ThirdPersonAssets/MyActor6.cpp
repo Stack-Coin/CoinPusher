1fd3cdd9fd863cc744bf87c462fa8cfb1538e8e1
Lim-Dolphin <imbq8603@gmail.com>
2026-08-31 17:21:01 +0900

    :t-rex: C++ Classes 폴더 생성
    - 각자 폴더에 Classes 만들고 MyActor(n) Dummy 파일은 지우세요~
    
    크앙~

---
+++ CP/Source/CP/ThirdPersonAssets/MyActor6.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonAssets/MyActor6.h"
+
+// Sets default values
+AMyActor6::AMyActor6()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor6::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor6::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
