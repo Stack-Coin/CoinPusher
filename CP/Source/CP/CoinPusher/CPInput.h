8638a79e8bf12110cbcde8d086dba947edcfa8a1
seopyo <seopyo26@gmail.com>
2026-09-01 18:33:19 +0900

    coin input test

--- CP/Source/CP/CoinPusher/CPInput.h
+++ CP/Source/CP/CoinPusher/CPInput.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "CPInteractable.h"
+#include "../Player/CPInteractable.h"
 #include "CPInput.generated.h"
 
 class UStaticMeshComponent;
+class USphereComponent;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInputInteracted, AActor*, Interactor);
 
@@ -18,6 +19,10 @@ class CP_API ACPInput : public AActor, public ICPInteractable
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* Mesh;
+
+	/** Overlap-only detection volume, since Mesh itself blocks (BlockAllDynamic) and won't fire overlaps */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
+	USphereComponent* CollisionSphere;
 
 public:
 	ACPInput();
@@ -30,6 +35,14 @@ public:
 	//ICPInteractable
 	virtual void Interact(AActor* Interactor) override;
 
+	/** Bound to CollisionSphere's OnComponentBeginOverlap. Registers this as an interact target while the player is in range */
+	UFUNCTION()
+	void OnCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/** Bound to CollisionSphere's OnComponentEndOverlap. Unregisters this once the player leaves range */
+	UFUNCTION()
+	void OnCollisionSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 protected:
 
 
@@ -38,6 +51,6 @@ protected:
 
 public:
 
-	//ÄÄÆ÷³ÍÆ® ¹ÝÈ¯
+	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ® ï¿½ï¿½È¯
 	FORCEINLINE UStaticMeshComponent* GetMesh() const { return Mesh; }
 };
