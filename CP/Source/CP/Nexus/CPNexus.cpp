db141253bc1f36c86a0d48ca280aeb257d1bd80f
R-lth <bluearth0502@gmail.com>
2026-09-03 12:08:40 +0900

    Nexus C++ 클래스 생성 및 SM에서 Collider 생성

---
+++ CP/Source/CP/Nexus/CPNexus.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Nexus/CPNexus.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ACPNexus::ACPNexus()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+}
+
+void ACPNexus::Dead() 
+{
+}
+
+float ACPNexus::TakeDamage(float DamageAmount, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	CurrentHp -= DamageAmount;
+
+	if (CurrentHp <= 0) 
+	{
+		Dead();
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Nexus: %f"), CurrentHp);
+	}
+
+	return DamageAmount;
+}
+
---
+++ CP/Source/CP/Nexus/CPNexus.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPNexus.generated.h"
+
+UCLASS()
+class CP_API ACPNexus : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACPNexus();
+
+public:
+	void Dead();
+	virtual float TakeDamage(float DamageAmount, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collider")
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	float MaxHp = 100.f;
+	float CurrentHp = 100.f;
+};
